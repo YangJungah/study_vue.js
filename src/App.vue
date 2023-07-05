764b6aef469eb5de15e97b0d32063f5a55493118
YangJungah <wjddk1914@naver.com>
2023-07-05 11:24:02 +0900

    커밋연습

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>정아의 쀼공부</h1>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
